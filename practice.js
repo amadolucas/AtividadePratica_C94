a23c51c0d90714c807b448e2e6f3a442d5e48ad5
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-12-01 13:55:52 -0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,6 +1,16 @@
 
 // ADICIONE SUS LINKS FIREBASE AQUI
+var firebaseConfig = {
+  apiKey: "AIzaSyA7EszbUgXdQWS_snLMP2srIa6bWyP3Y6M",
+  authDomain: "testeaula-25aa9.firebaseapp.com",
+  projectId: "testeaula-25aa9",
+  storageBucket: "testeaula-25aa9.appspot.com",
+  messagingSenderId: "345316708710",
+  appId: "1:345316708710:web:8f2f1bedef2b41f0d8f8a7"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 function addUser()
 {
   userName = document.getElementById("userName").value;
